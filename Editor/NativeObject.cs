53e81cf536e10796c2ace52ff63d6a46ded51114
Benjamin Moir <DaZombieKiller@users.noreply.github.com>
2022-02-27 16:27:27 +1000

    Implement NativeObject.GetNativeObject

--- Editor/NativeObject.cs
+++ Editor/NativeObject.cs
@@ -12,16 +12,26 @@ namespace NewBlood
             .GetMethod("GetOffsetOfInstanceIDInCPlusPlusObject", BindingFlags.NonPublic | BindingFlags.Static)
             .Invoke(null, null);
 
+        static readonly Func<Object, IntPtr> s_GetCachedPtr = (Func<Object, IntPtr>)typeof(Object)
+            .GetMethod("GetCachedPtr", BindingFlags.NonPublic | BindingFlags.Instance)
+            .CreateDelegate(typeof(Func<Object, IntPtr>));
+
         /// <summary>Gets the instance ID of the provided native object.</summary>
-        public static int GetInstanceID(IntPtr address)
+        public static int GetInstanceID(IntPtr native)
         {
-            return *(int*)IntPtr.Add(address, s_OffsetOfInstanceIDInCPlusPlusObject);
+            return *(int*)IntPtr.Add(native, s_OffsetOfInstanceIDInCPlusPlusObject);
         }
 
-        /// <summary>Gets the managed wrapper object associated with the provided native object.</summary>
-        public static Object GetManagedObject(IntPtr address)
+        /// <summary>Gets the managed wrapper associated with the provided native object.</summary>
+        public static Object GetManagedObject(IntPtr native)
         {
-            return EditorUtility.InstanceIDToObject(GetInstanceID(address));
+            return EditorUtility.InstanceIDToObject(GetInstanceID(native));
+        }
+
+        /// <summary>Returns the underlying native object from a managed wrapper.</summary>
+        public static IntPtr GetNativeObject(Object managed)
+        {
+            return s_GetCachedPtr(managed);
         }
     }
 }
